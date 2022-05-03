a9b1946aed5afff60fd8e8680f3a1ee5b8986b00
powerhorang2 <tjrdbqls3@naver.com>
2022-05-03 10:51:45 +0900

    링크드 리스트 노드 추가 오퍼레이션 및 검색 오퍼레이션, toString 추가

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -19,4 +19,87 @@ public class LinkedList {
             return String.valueOf(this.data);
         }
     }
+
+    public void addFirst(Object input) {
+        // 노드를 생성합니다.
+        Node newNode = new Node(input);
+        // 새로운 노드의 다음 노드로 헤드를 지정합니다.
+        newNode.next = head;
+        // 헤드로 새로운 노드를 지정합니다.
+        head = newNode;
+        size++;
+        if (head.next == null) {
+            tail = head;
+        }
+    }
+
+    public void addLast(Object input) {
+        // 노드를 생성합니다.
+        Node newNode = new Node(input);
+        // 리스트의 노드가 없다면 첫번째 노드를 추가하는 메소드를 사용합니다.
+        if (size == 0) {
+            addFirst(input);
+        } else {
+            // 마지막 노드의 다음 노드로 생성한 노드를 지정합니다.
+            tail.next = newNode;
+            // 마지막 노드를 갱신합니다.
+            tail = newNode;
+            // 엘리먼트의 개수를 1 증가 시킵니다.
+            size++;
+        }
+    }
+
+    Node node(int index) {
+        Node x = head;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    public void add(int k, Object input) {
+        // 만약 k가 0이라면 첫번째 노드에 추가하는 것이기 때문에 addFirst 를 사용합니다.
+        if (k == 0) {
+            addFirst(input);
+        } else {
+            // k 번째 이전의 노드를 temp1 로 지정
+            Node temp1 = node(k-1);
+            // k 번째 노드를 temp2 로 지정
+            Node temp2 = temp1.next;
+            // 새로운 노드를 생성합니다.
+            Node newNode = new Node(input);
+            // temp1 의 다음 노드로 새로운 노드를 지정합니다.
+            temp1.next = newNode;
+            // 새로운 노드의 다음 노드로 temp2 를 지정합니다.
+            newNode.next = temp2;
+            size++;
+            // 새로운 노드의 다음 노드가 없다면 새로운 노드가 마지막 노드이기 때문에 tail 로 지정합니다.
+            if (newNode.next == null) {
+                tail = newNode;
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        // 노드가 없다면 []를 리턴합니다.
+        if(head == null) {
+            return "[]";
+        }
+
+        // 탐색을 시작합니다.
+        Node temp = head;
+        String str = "[";
+        // 다음 노드가 없을 때까지 반복문을 실행합니다.
+        // 마지막 노드는 다음 노드가 없기 때문에 아래의 구문에서 마지막 노드는 제외됩니다.
+        while (temp.next != null) {
+            str += temp.data + ", ";
+            temp = temp.next;
+        }
+
+        // 마지막 노드를 출력 결과에 포함시킵니다.
+        str += temp.data;
+        return str + "]";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        LinkedList numbers = new LinkedList();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addFirst(5);
+        numbers.add(3, 25);
+        System.out.println("numbers = " + numbers);
     }
 }
